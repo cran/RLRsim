6006829d74fdb45e54684c047fb3e09cccc6c5f0
Fabian Scheipl <fabian.scheipl@stat.uni-muenchen.de>
2013-02-21 00:00:00 +0000

    version 2.1-0

--- R/exactLRT.R
+++ R/exactLRT.R
@@ -1,61 +1,70 @@
 `exactLRT` <-
-		function(m, m0, seed = NA, nsim = 10000, 
-				log.grid.hi = 8, log.grid.lo = -10, gridlength = 200) 
-{
-	if (class(m0) != "lm") 
-		stop("m0 not an lm-object. \n")
-	if (class(m) == "spm") {
-		m <- m$fit
-		class(m) <- "lme"
-	}
-	if (class(m) == "amer") 
-		class(m) <- "mer"
-	if (!((c.m <- class(m)) %in% c("mer", "lme", "lmerMod"))) 
-		stop("Invalid m specified. \n")
-	if(c.m == "mer"){
-		if(length(m@muEta))
-			stop("exactLRT can only be used for mixed models for Gaussian responses.")
-	}
-	
-	d <- switch(c.m, lme = extract.lmeDesign(m), mer = extract.lmerDesign(m), lmerMod=extract.lmerModDesign(m))
-	if(length(d$lambda) != 1 || d$k != 1) 
-		stop("multiple random effects in model - exactLRT needs 'm' with only a single random effect.")
-	X <- d$X
-	Z <- d$Z
-	y <- d$y
-	Vr <- d$Vr
-	K <- NCOL(Z)
-	n <- NROW(X)
-	p <- NCOL(X)
-	q <- p - length(coefficients(m0)[!is.na(coefficients(m0))])
-	if (n != length(m0$fitted)) 
-		stop("different data under the null and alternative. \n")
-	if (q < 0) 
-		stop("m0 not nested in m. \n")
-	if (n - p - K < 1) 
-		stop("No. of effects greater than n. Reduce model complexity.\n")
-	if (q == 0) 
-		cat("No restrictions on fixed effects. REML-based inference preferable. \n")
-	method <- switch(c.m, lme = m$method, 
-			mer = ifelse(is.null(m@call$REML), "REML", ifelse(eval(m@call$REML), "REML", "ML")))
-	if (method != "ML") {
-		cat("Using likelihood evaluated at REML estimators.\nPlease refit model with method=\"ML\" for exact results.\n")
-	}
-	#observed value of the LRT
-	lrt.obs <- max(0, 2 * logLik(m, REML = FALSE)[1] - 2 * logLik(m0, 
-					REML = FALSE)[1])
-	sample <- LRTSim(X, Z, q, sqrt.Sigma = chol(cov2cor(Vr)), 
-			seed = seed, nsim = nsim, log.grid.hi = log.grid.hi, 
-			log.grid.lo = log.grid.lo, gridlength = gridlength)
-	if (quantile(sample, 0.9) == 0) 
-		(cat("Warning: Null distribution has", mean(sample == 
-											0), "mass at zero.\n"))
-	p <- mean(lrt.obs < sample)
-	RVAL <- list(statistic = c(LRT = lrt.obs), 
-            p.value = p, 
-            method = paste("simulated finite sample distribution of LRT. (p-value based on", 
-					nsim, "simulated values)"), sample=sample)
-	class(RVAL) <- "htest"
-	return(RVAL)
-} 
+  function(m, m0, seed = NA, nsim = 10000, 
+           log.grid.hi = 8, log.grid.lo = -10, gridlength = 200,
+           parallel = c("no", "multicore", "snow"), 
+           ncpus = 1L, cl = NULL) 
+  {
+    if (class(m0) != "lm") 
+      stop("m0 not an lm-object. \n")
+    if (class(m) == "spm") {
+      m <- m$fit
+      class(m) <- "lme"
+    }
+    if (class(m) == "amer") 
+      class(m) <- "mer"
+    if (!((c.m <- class(m)) %in% c("mer", "lme", "lmerMod"))) 
+      stop("Invalid m specified. \n")
+    if(c.m == "mer"){
+      if(length(m@muEta))
+        stop("exactLRT can only be used for mixed models for Gaussian responses.")
+    }
+    
+    d <- switch(c.m, lme = extract.lmeDesign(m), 
+                mer = extract.lmerDesign(m), 
+                lmerMod=extract.lmerModDesign(m))
+    if(length(d$lambda) != 1 || d$k != 1) 
+      stop("multiple random effects in model - exactLRT needs 'm' with only a single random effect.")
+    X <- d$X
+    Z <- d$Z
+    y <- d$y
+    Vr <- d$Vr
+    K <- NCOL(Z)
+    n <- NROW(X)
+    p <- NCOL(X)
+    q <- p - length(coefficients(m0)[!is.na(coefficients(m0))])
+    if (n != length(m0$fitted)) 
+      stop("different data under the null and alternative. \n")
+    if (q < 0) 
+      stop("m0 not nested in m. \n")
+    if (n - p - K < 1) 
+      stop("No. of effects greater than n. Reduce model complexity.\n")
+    if (q == 0) 
+      message("No restrictions on fixed effects. REML-based inference preferable.")
+    method <- switch(c.m, lme = m$method, 
+                     mer = ifelse(is.null(m@call$REML),
+                                  "REML", ifelse(eval(m@call$REML), "REML", "ML")))
+    if (method != "ML") {
+      message("Using likelihood evaluated at REML estimators.")
+      message("Please refit model with method=\"ML\" for exact results.")
+    }
+    #observed value of the LRT
+    lrt.obs <- max(0, 2 * logLik(m, REML = FALSE)[1] - 2 * logLik(m0, 
+                                                                  REML = FALSE)[1])
+    sample <- LRTSim(X, Z, q, sqrt.Sigma = chol(cov2cor(Vr)), 
+                     seed = seed, nsim = nsim, log.grid.hi = log.grid.hi, 
+                     log.grid.lo = log.grid.lo, gridlength = gridlength,
+                     parallel = match.arg(parallel), 
+                     ncpus = ncpus, cl = cl)
+    if (quantile(sample, 0.9) == 0) {
+      warning("Null distribution has", mean(sample == 
+        0), "mass at zero.\n")
+    }
+    p <- mean(lrt.obs < sample)
+    RVAL <- list(statistic = c(LRT = lrt.obs), 
+                 p.value = p, 
+                 method = paste("simulated finite sample distribution of LRT. (p-value based on", 
+                                nsim, "simulated values)"), sample=sample)
+    class(RVAL) <- "htest"
+    return(RVAL)
+  } 
 
--- R/exactRLRT.R
+++ R/exactRLRT.R
@@ -1,80 +1,89 @@
 'exactRLRT' <- function(m, mA = NULL, m0 = NULL, seed = NA, 
-		nsim = 10000, log.grid.hi = 8, log.grid.lo = -10, gridlength = 200) {
-	if (class(m) == "spm") {
-		m <- m$fit
-		class(m) <- "lme"
-	}
-	if (class(m) == "amer") class(m) <- "mer"
-	if (!(c.m <- (class(m))) %in% c("mer", "lme", "lmerMod")) 
-		stop("Invalid m specified. \n")
-	if(c.m == "mer"){
-		if(length(m@muEta))
-			stop("exactRLRT can only be used for mixed models for Gaussian responses.")
-	}
-	if ("REML" != switch(c.m, 
-			lme = m$method, 
-			mer = switch(m@dims["REML"] + 1, "ML", "REML"),
-            lmerMod = if (isREML(m)) "REML" else "ML")){
-		cat("Using restricted likelihood evaluated at ML estimators.\n Refit with method=\"REML\" for exact results.\n")	
-	}
-	
-	d <- switch(c.m, lme = extract.lmeDesign(m), mer = extract.lmerDesign(m), lmerMod = extract.lmerModDesign(m))
-	X <- d$X
-	qrX <- qr(X)
-	Z <- d$Z
-	y <- d$y
-	Vr <- d$Vr
-	K <- ncol(Z)
-	n <- nrow(X)
-	p <- ncol(X)
-	if (is.null(mA) && is.null(m0)) {
-		if(length(d$lambda) != 1 || d$k != 1) 
-			stop("multiple random effects in model - exactRLRT needs 'm' with only a single random effect.")
-		#2*restricted ProfileLogLik under H0: lambda=0
-		res <- qr.resid(qrX, y)
-		R <- qr.R(qrX)
-		detXtX <- det(t(R) %*% R)
-		reml.H0 <- -((n - p) * log(2 * pi) + (n - p) * log(sum(res^2)) + 
-					log(detXtX) + (n - p) - (n - p) * log(n - p))
-		#observed value of the test-statistic
-		reml.obs <- 2 * logLik(m, REML = TRUE)[1]
-		rlrt.obs <- max(0, reml.obs - reml.H0)
-		lambda <- d$lambda
-	}
-	else {
-        nonidentfixmsg <- "Fixed effects structures of mA and m0 not identical.\n REML-based inference not appropriate."
-		if (c.m == "lme") {
-			if (any(mA$fixDF$terms != m0$fixDF$terms)) 
-				stop(nonidentfixmsg)
-		} else {
-            if (c.m == "mer") {
-                if (any(mA@X != m0@X)) 
-                    stop(nonidentfixmsg)
-            } else {
-                if (c.m == "lmerMod") {
-                    if (any(getME(mA,"X") != getME(m0,"X")))
-                        stop(nonidentfixmsg)
-                }
-            }     
-		}
-		## bug fix submitted by Andrzej Galecki 3/10/2009
-		DFx <- switch(c.m, lme = anova(mA,m0)$df, mer = anova(mA,m0)$Df, lmerMod=anova(mA,m0)$Df) 
-		if (abs(diff(DFx)) > 1) {
-			stop("Random effects not independent - covariance(s) set to 0 under the null hypothesis.\n exactRLRT can only test a single variance.\n")
-		}
-		rlrt.obs <- max(0, 2 * (logLik(mA, REML = TRUE)[1] - 
-							logLik(m0, REML = TRUE)[1]))
-	}
-	if (rlrt.obs != 0) {
-		sample <- RLRTSim(X, Z, qrX=qrX, sqrt.Sigma = chol(cov2cor(Vr)), 
-				lambda0 = 0, seed = seed, nsim = nsim, log.grid.hi = log.grid.hi, 
-				log.grid.lo = log.grid.lo, gridlength = gridlength)
-		p <- mean(rlrt.obs < sample)
-	}
-	else p = 1
-	RVAL <- list(statistic = c(RLRT = rlrt.obs), p.value = p, 
-			method = paste("simulated finite sample distribution of RLRT.\n (p-value based on", 
-					nsim, "simulated values)"), sample=sample)
-	class(RVAL) <- "htest"
-	return(RVAL)
+                        nsim = 10000, log.grid.hi = 8, log.grid.lo = -10, gridlength = 200,
+                        parallel = c("no", "multicore", "snow"), 
+                        ncpus = 1L, cl = NULL) {
+  if (class(m) == "spm") {
+    m <- m$fit
+    class(m) <- "lme"
+  }
+  if (class(m) == "amer") class(m) <- "mer"
+  if (!(c.m <- (class(m))) %in% c("mer", "lme", "lmerMod")) 
+    stop("Invalid m specified. \n")
+  if(c.m == "mer"){
+    if(length(m@muEta))
+      stop("exactRLRT can only be used for mixed models for Gaussian responses.")
+  }
+  if ("REML" != switch(c.m, 
+                       lme = m$method, 
+                       mer = switch(m@dims["REML"] + 1, "ML", "REML"),
+                       lmerMod = if (isREML(m)) "REML" else "ML")){
+    message("Using restricted likelihood evaluated at ML estimators.")
+    message("Refit with method=\"REML\" for exact results.")	
+  }
+  
+  d <- switch(c.m, lme = extract.lmeDesign(m), 
+              mer = extract.lmerDesign(m),
+              lmerMod = extract.lmerModDesign(m))
+  X <- d$X
+  qrX <- qr(X)
+  Z <- d$Z
+  y <- d$y
+  Vr <- d$Vr
+  K <- ncol(Z)
+  n <- nrow(X)
+  p <- ncol(X)
+  if (is.null(mA) && is.null(m0)) {
+    if(length(d$lambda) != 1 || d$k != 1) 
+      stop("multiple random effects in model - exactRLRT needs 'm' with only a single random effect.")
+    #2*restricted ProfileLogLik under H0: lambda=0
+    res <- qr.resid(qrX, y)
+    R <- qr.R(qrX)
+    detXtX <- det(t(R) %*% R)
+    reml.H0 <- -((n - p) * log(2 * pi) + (n - p) * log(sum(res^2)) + 
+      log(detXtX) + (n - p) - (n - p) * log(n - p))
+    #observed value of the test-statistic
+    reml.obs <- 2 * logLik(m, REML = TRUE)[1]
+    rlrt.obs <- max(0, reml.obs - reml.H0)
+    lambda <- d$lambda
+  } else {
+    nonidentfixmsg <- 
+      "Fixed effects structures of mA and m0 not identical.\n REML-based inference not appropriate."
+    if (c.m == "lme") {
+      if (any(mA$fixDF$terms != m0$fixDF$terms)) 
+        stop(nonidentfixmsg)
+    } else {
+      if (c.m == "mer") {
+        if (any(mA@X != m0@X)) 
+          stop(nonidentfixmsg)
+      } else {
+        if (c.m == "lmerMod") {
+          if (any(getME(mA,"X") != getME(m0,"X")))
+            stop(nonidentfixmsg)
+        }
+      }     
+    }
+    ## bug fix submitted by Andrzej Galecki 3/10/2009
+    DFx <- switch(c.m, lme = anova(mA,m0)$df, 
+                  mer = anova(mA,m0)$Df,
+                  lmerMod=anova(mA,m0)$Df) 
+    if (abs(diff(DFx)) > 1) {
+      stop("Random effects not independent - covariance(s) set to 0 under the null hypothesis.\n exactRLRT can only test a single variance.\n")
+    }
+    rlrt.obs <- max(0, 2 * (logLik(mA, REML = TRUE)[1] - 
+      logLik(m0, REML = TRUE)[1]))
+  }
+  if (rlrt.obs != 0) {
+    sample <- RLRTSim(X, Z, qrX=qrX, sqrt.Sigma = chol(cov2cor(Vr)), 
+                      lambda0 = 0, seed = seed, nsim = nsim, log.grid.hi = log.grid.hi, 
+                      log.grid.lo = log.grid.lo, gridlength = gridlength, 
+                      parallel = match.arg(parallel), 
+                      ncpus = ncpus, cl = cl)
+    p <- mean(rlrt.obs < sample)
+  }
+  else p = 1
+  RVAL <- list(statistic = c(RLRT = rlrt.obs), p.value = p, 
+               method = paste("simulated finite sample distribution of RLRT.\n (p-value based on", 
+                              nsim, "simulated values)"), sample=sample)
+  class(RVAL) <- "htest"
+  return(RVAL)
 } 
